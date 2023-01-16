f2db4475aec169e3ddfc035fd8e79021069aa528
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-16 22:53:17 +0100

    [book_label_implement_method]: Add: List_all methods

--- src/books.rb
+++ src/books.rb
@@ -9,6 +9,20 @@ class Book < Item
     @cover_state = cover_state
   end
 
+  def self.list_all(books)
+    if books.empty?
+      puts 'Your Books list is empty'
+    else
+      puts "Books list, count(#{books.count})📚 :\n\n"
+      books.each_with_index do |book, index|
+        puts "#{index + 1}) Title: '#{book.label.title}'",
+             "   Author: #{book.author.first_name}, #{book.author.last_name} ",
+             "   Publisher: #{book.publisher}",
+             "   Cover State: #{book.cover_state}"
+      end
+    end
+  end
+
   private
 
   def can_be_archived?
