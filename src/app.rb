d282bd040f893d924edf2ce364ee7f9a629f2c0e
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-16 15:51:14 +0100

    [items-feature]: Create: App class and methods

--- src/app.rb
+++ src/app.rb
@@ -1,2 +1,40 @@
 class App
+
+  def list_all_books
+    puts "books Listed"
+  end
+  def list_all_music_albums
+    puts "Music Album Listed"
+  end
+  def list_all_games
+    puts "games Listed"
+  end
+
+  def list_all_genres
+    puts "Genre Listed"
+  end
+
+  def list_all_labels
+    puts "Label Listed"
+  end
+
+  def list_all_authors
+    puts "Author Listed"
+  end
+
+  def add_book
+    puts "Book Added"
+  end
+
+  def add_music_album
+    puts "music Added"
+  end
+
+  def add_game
+    puts "Game Added"
+  end
+
+  def quit_app
+    exit
+  end
 end
