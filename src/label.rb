6344fdf8e4e242e8467edb8d9ff024cb97103564
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-16 22:53:34 +0100

    [book_label_implement_method]: Add: List_all methods

--- src/label.rb
+++ src/label.rb
@@ -11,6 +11,17 @@ class Label
     @items = []
   end
 
+  def self.list_all(labels)
+    if labels.empty?
+      puts 'Your label list is empty'
+    else
+      puts "Labels list, count(#{labels.count})🏷️ :\n\n"
+      labels.each_with_index do |label, index|
+        puts "#{index + 1}) Title: '#{label.title}', Color: #{label.color}"
+      end
+    end
+  end
+
   def add_item(item)
     @items << item
     item.label = self
