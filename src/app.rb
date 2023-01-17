dde2c29147caca75dd1ed5d9fd23b1b164cf71a4
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-17 09:17:19 +0100

    [book_label_implement_method]: Add: addbook method

--- src/app.rb
+++ src/app.rb
@@ -1,3 +1,5 @@
+require_relative './label'
+
 class App
   def list_all_books
     puts 'books Listed'
@@ -23,8 +25,43 @@ class App
     puts 'Author Listed'
   end
 
+  def add_label(thing)
+    print "Title of the #{thing}: "
+    title = gets.chomp
+    print "Color of the #{thing}: "
+    color = gets.chomp
+    Label.new(title, color)
+  end
+
+  def add_author
+    #Author Properties
+  end
+
+  def add_genre(thing)
+    #thing can be Book, music or game
+    #genre Properties
+  end
+
+
   def add_book
-    puts 'Book Added'
+    label = add_label('Book')
+    author = add_author
+    genre = add_genre('book')
+    print 'What\'s the state of the Book Cover? [good/bad]: '
+    cover_state = gets.chomp.downcase
+    print 'Insert Book Publisher? '
+    publisher = gets.chomp
+    print 'Insert Publishing date? [year/month/day] (e.g 1937/11/12): '
+    published_date = gets.chomp
+    book = Book.new(publisher, cover_state, published_date)
+    label.add_item(book)
+    genre.add_item(book)
+    author.add_item(book)
+    @books << book
+    @labels << label
+    @genres << genre
+    @authors << author
+    puts "\n The book '#{label.title}' by #{author.first_name} #{author.last_name} was created successfully!✅ "
   end
 
   def add_music_album
@@ -34,6 +71,7 @@ class App
   def add_game
     puts 'Game Added'
   end
+
 
   def quit_app
     exit
