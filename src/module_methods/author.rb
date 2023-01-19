35291858d1983fab2c2e1a7c5b2ed2f33e953a7d
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-19 19:06:48 +0100

    add: Author module

---
+++ src/module_methods/author.rb
@@ -0,0 +1,30 @@
+require_relative '../author'
+require_relative '../storage_helper_methods/storage'
+
+module AuthorModule
+  include Storage
+
+  def store_author(authors)
+    author = []
+    authors.each do |item|
+      author << {
+        id: item.id,
+        first_name: item.first_name,
+        last_name: item.last_name,
+      }
+    end
+    return if authors.empty?
+
+    update_data(author, './src/data/author.json')
+  end
+
+  def read_authors
+    authors = []
+    data = get_data('./src/data/authors.json')
+    data.each do |item|
+      author = Author.new(item['first_name'], item['last_name'], item['id'])
+      authors << author
+    end
+    authors
+  end
+end
